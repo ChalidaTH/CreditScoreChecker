55fecedc5c68746fddb3510d6dfa14b35b924575
ChalidaTH <127090081+ChalidaTH@users.noreply.github.com>
2023-03-29 23:20:42 +0800

    Update 2_Streamlit_score_classifier.py

--- 2_Streamlit_score_classifier.py
+++ 2_Streamlit_score_classifier.py
@@ -1,10 +1,10 @@
 # Import Libraries
 import streamlit as st
 import pandas as pd
-#from sklearn import model_selection
-#from sklearn.model_selection import train_test_split
-#from sklearn.ensemble import RandomForestClassifier
-#from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn import model_selection
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import MinMaxScaler  # Add this line
 
 # Set Page configuration
 # Read more at https://docs.streamlit.io/1.6.0/library/api-reference/utilities/st.set_page_config
@@ -25,31 +25,28 @@ Amount = st.sidebar.slider('4. Please state the amount needed for your housing l
 Front = st.sidebar.slider('5. Please state the expected mortgage principal, interest and housing payment to your income (%)', 0, 20, 100)
 Back = st.sidebar.slider('6. Please state total debt including housing payment to your income (%)', 0, 30, 100)
 
-# Separate to X and y
-#X = df.drop('BoCreditScore', axis=1)
-#y = df.BoCreditScore
-
-# Split data
-#X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2, random_state=42)
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(df[['IncomePerBo','UPB','Amount','Front','Back','First']],
+                                                    df['BoCreditScore'], test_size=0.2, random_state=42)
 
 # Normalize the data
-#scaler = MinMaxScaler()
-#X_train = scaler.fit_transform(X_train)
-#X_test = scaler.transform(X_test)
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
-# Build model
-#model = RandomForestClassifier(n_estimators=50, max_depth=10, min_samples_split=2, random_state=42)
-#model.fit(X_train, y_train)
+# Train the model using the best hyperparameters
+model = RandomForestClassifier(n_estimators=50, max_depth=10, min_samples_split=2, random_state=42)
+model.fit(X_train, y_train)
 
-# Generate prediction based on user selected attributes
-#y_pred = model.predict([[IncomePerBo, UPB, Amount, Front, Back, First]])
+# Make predictions on the testing set
+y_pred = model.predict(X_test)
 
 # Display EDA
-# st.subheader('Exploratory Data Analysis')
-# st.write('The data is grouped by the credit score class and the variable mean is computed for each class.')
-# groupby_species_mean = df.groupby('BoCreditScore').mean()
-# st.write(groupby_species_mean)
-# st.bar_chart(groupby_species_mean.T)
+st.subheader('Exploratory Data Analysis')
+st.write('The data is grouped by the credit score class and the variable mean is computed for each class.')
+groupby_species_mean = df.groupby('BoCreditScore').mean()
+st.write(groupby_species_mean)
+st.bar_chart(groupby_species_mean.T)
 
 # credit score table
 Score_table = {'Credit Score Group': ['5', '4', '3', '2','1'],
@@ -65,4 +62,4 @@ st.table(score_df)
 
 # Print predicted flower species
 st.subheader('Prediction')
-#st.metric('Predicted Credit Score class is :', y_pred[0], '')
+st.metric('Predicted Credit Score class is :', y_pred[0], '')
