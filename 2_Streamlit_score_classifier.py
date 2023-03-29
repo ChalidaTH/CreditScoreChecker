0e054eaa5b70f5451131b688870d970c6bbdae09
ChalidaTH <127090081+ChalidaTH@users.noreply.github.com>
2023-03-29 23:50:23 +0800

    Update 2_Streamlit_score_classifier.py

--- 2_Streamlit_score_classifier.py
+++ 2_Streamlit_score_classifier.py
@@ -1,6 +1,8 @@
 # Import Libraries
 import streamlit as st
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 #from sklearn.model_selection import train_test_split
 #from sklearn import model_selection
 #from sklearn.ensemble import RandomForestClassifier
