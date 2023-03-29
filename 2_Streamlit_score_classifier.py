ed33aeaaa6cfa0b9a86d95e94ca7bc30e70b2b3c
ChalidaTH <127090081+ChalidaTH@users.noreply.github.com>
2023-03-29 23:44:01 +0800

    Update 2_Streamlit_score_classifier.py

--- 2_Streamlit_score_classifier.py
+++ 2_Streamlit_score_classifier.py
@@ -57,7 +57,7 @@ st.write(groupby_species_mean)
 score_df = pd.DataFrame(Score_table)
 
 # Display the dataframe in a table using Streamlit
-st.table(score_df)
+st.table(score_df, index=False)
 
 
 # Print predicted flower species
