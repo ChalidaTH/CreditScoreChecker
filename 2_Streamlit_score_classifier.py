0cbde01a787f40ae5e521d96bb890227396b8766
ChalidaTH <127090081+ChalidaTH@users.noreply.github.com>
2023-03-30 14:33:51 +0800

    Update 2_Streamlit_score_classifier.py

--- 2_Streamlit_score_classifier.py
+++ 2_Streamlit_score_classifier.py
@@ -74,10 +74,7 @@ groupby_species_mean = groupby_species_mean.rename(columns={
     "BoCreditScore": "Borrowers' credit score",
     "IncomePerBo": "Income",
     "UPB": "Unpaid loan balance",
-    "Amount": "Housing loan amount",
-    "Front": "Housing payment to income ratio",
-    "Back": "Debt payment to income ratio",
-    "First": "First time home owner"
+    "Amount": "Housing loan amount"
 })
 
 st.write(groupby_species_mean)
